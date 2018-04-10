e8db7c8a67add0b15992405940b907edb64bc83a
Usagirei <usagirei@users.noreply.github.com>
2018-04-09 22:53:52 -0300

    Add Image Attribute to Checkbox

--- JankyUI/Nodes/Widgets/CheckboxNode.cs
+++ JankyUI/Nodes/Widgets/CheckboxNode.cs
@@ -9,6 +9,7 @@ namespace JankyUI.Nodes
 {
     [JankyTag("Checkbox")]
     [JankyProperty("text", nameof(Text))]
+    [JankyProperty("image", nameof(Image))]
     [JankyProperty("checked", nameof(Checked))]
     [JankyProperty("on-change", nameof(OnChange))]
     internal class CheckboxNode : LayoutNode
@@ -17,14 +18,29 @@ namespace JankyUI.Nodes
 
         public JankyProperty<bool> Checked;
         public JankyProperty<string> Text;
+        public JankyProperty<Texture> Image;
+
+        private readonly GUIContent Content;
+
+        public CheckboxNode()
+        {
+            Content = new GUIContent();
+        }
+
+        private void UpdateContent()
+        {
+            Content.text = Text;
+            Content.image = Image;
+        }
 
         protected override void OnGUI()
         {
 #if MOCK
             Console.WriteLine("Checkbox: {0} {1}", Text, Checked);
 #else
+            UpdateContent();
             bool oldValue = Checked;
-            var newValue = GUILayout.Toggle(oldValue, Text, GetLayoutOptions());
+            var newValue = GUILayout.Toggle(oldValue, Content, GetLayoutOptions());
 
             Checked.Value = newValue;
             if (Checked.LastSetResult != DataOperationResultEnum.Unchanged)
